472346d43e1dae6550d5e11d7f5a8d20fafddaf8
Alireza <Changiz.and.sony@gmail.com>
2024-11-22 03:05:34 +0100

    Feat : Added the Analyzer and CW Check in sync

--- TS(cli).py
+++ TS(cli).py
@@ -23,10 +23,21 @@ class TextAnalytics:
                     break
             self.Data[Errorpath] = input(f"the {Errorpath} is incorrect. please rewrite the path: ")
             self.Check()
-    def IgnoreWords(self):
-        if self.IgnoreFileEmpty is not True:
+    def IgnoreWord(self):
+        if self.IgnoreFileEmpty is True:
             with open(self.Data["IWFP"], "r") as IW:
-                self.IgnoreWords = IW.read().split(" ")
+                self.IgnoreWords = IW.read().split("\n")
+    def ConsecutiveWords(self):
+        for i in range(0,len(self.InputText),self.CW):
+            j = i+(i+self.CW)
+            tmp = self.InputText.split(" ")
+            tmp[i].join(tmp[j])
+            continue
+    def Analyzer(self):
+        with open(self.Data["IFP"],"r") as IF:
+            self.InputText = IF
+            self.IgnoreWord()
+            self.ConsecutiveWords()
 
             
 

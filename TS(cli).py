a7f9cebbaaa270729822788a1c004218cc06eec0
Alireza <Changiz.and.sony@gmail.com>
2024-11-22 02:09:24 +0100

    Bug Fix : Made the data handling easier and cleaner

--- TS(cli).py
+++ TS(cli).py
@@ -1,19 +1,24 @@
 from os import pathsep
 class TextAnalytics:
-    def __init__(IFP,OFP,IWFP,CW=1):
-        try:
-            open(file=IFP)
-            open(file=OFP)
-            open(file=IWFP)
-
-        except FileNotFoundError:
-            # the thing in here is it'll take all parametrs even if one is wrong for not overengineering didint change it
-            print("The entered file path does not exist please enter a valid path name")
-            self.IFP = input("enter the correct input file path :")
-            self.OFP = input("enter the correct output file path :")
-            self.IWFP = input("enter the correct ignored words file path :")
-        else:
-            self.IFP = IFP
-            self.OFP = OFP
-            self.IWFP = IWFP
+    def __init__(self,IFP=str,OFP=str,IWFP="",CW=1):
+        self.Checked = False
+        self.Data = {"IFP" : IFP ,"OFP" : OFP ,"IWFP" : IWFP};
         self.CW = CW
+        if self.Checked is False:
+            self.Check()
+    def Check(self):
+        Errorpath = ""
+        try:
+            open(self.Data["IFP"],"r")
+            open(self.Data["OFP"],"r")
+            if self.Data["IWFP"] == "":
+                pass
+            else:
+                open(self.Data["IWFP"],"r")
+        except FileNotFoundError as i:
+            for j in self.Data.items():
+                if j[1] == i.filename:
+                    Errorpath = j[0]
+                    break
+            self.Data[Errorpath] = input(f"the {Errorpath} is incorrect. please rewrite the path: ")
+            self.Check()
